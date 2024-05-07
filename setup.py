0c692e31999772c5a0f87e942c34aa512233c7ec
Jack Cavaluzzi <Jack.Cavaluzzi@inl.gov>
2024-05-07 11:41:00 -0600

    adding deeplynx-py-sdk dependency

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='airflow-provider-deeplynx',
+    version='0.1.0',
+    packages=['deeplynx_provider'],
+    install_requires=[
+        'apache-airflow>=2.4',
+        'deep_lynx @ git+https://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@github.inl.gov/Digital-Engineering/deeplynx-py-sdk.git@main'
+    ],
+    # Any other package-specific setuptools configurations
+)
